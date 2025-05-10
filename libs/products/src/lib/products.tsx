7cbb4f110dc618b414492df79812367972acd540
Suhas <suhas.gavas@gmail.com>
2025-05-10 14:08:53 +0530

    updates

---
+++ libs/products/src/lib/products.tsx
@@ -0,0 +1,13 @@
+import styles from './products.module.scss';
+import { Orders } from '@react-monorepo/orders';
+
+export function Products() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to Products!</h1>
+      <p>This is a change. 👋</p>
+    </div>
+  );
+}
+
+export default Products;
